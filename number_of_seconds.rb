1e6924283a6f7da22ecab5590b15e2042a1b06e8
Nick Vass <prkarpi@hotmail.com>
2016-02-23 19:25:46 -0700

    homework three master
    [#114279483]

---
+++ number_of_seconds.rb
@@ -0,0 +1,16 @@
+#!usr/bin/env ruby
+
+seconds_in_a_minute = 60
+minutes_in_an_hour = 60
+hours_in_a_day = 24
+days_in_the_week = 7
+my_age = 18
+days_in_the_year = 365.25 # with leap year
+
+puts seconds_in_a_minute * minutes_in_an_hour
+
+puts seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day
+
+puts seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_the_week
+
+puts my_age * days_in_the_year * seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day
