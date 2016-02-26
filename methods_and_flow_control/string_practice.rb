6a0e71fe5929408f6adfc26beba667d63bc460ca
Nick Vass <prkarpi@hotmail.com>
2016-02-25 19:21:03 -0700

    homework 1: methods and flow control
    [#114371515]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -8,4 +8,7 @@ puts variable_with_string.upcase
 puts variable_with_string.gsub("string", "'string'")
 puts "this string \'this is a string to practice with has\' #{variable_with_string.length} characters"
 puts variable_with_string.reverse
-puts variable_with_string.gsub("practice", )
+my_string = variable_with_string
+if my_string["practice"]
+  puts "practise " * 3
+end
