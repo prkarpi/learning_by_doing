4002b720439cf24477d24c0002c1bd443ceadf0d
Nick Vass <prkarpi@hotmail.com>
2016-03-06 14:28:25 -0700

    homework 2: daily basis example - daily_basis_example.rb
    [#115098917]

---
+++ objects/daily_basis_example.rb
@@ -0,0 +1,27 @@
+#!usr/bin/env ruby
+
+class DailyDrivingMiles
+
+  def initialize
+    @total_miles = 0
+end
+
+  def miles_from_home_to_work
+    @work = 9.25
+  end
+
+  def miles_from_work_to_davinci
+    @davinci = 10.5
+  end
+
+  def miles_from_davinci_to_home
+    @davinci + @work
+  end
+  
+end
+
+count_miles = DailyDrivingMiles.new
+
+puts "Driving miles from my home to work: #{count_miles.miles_from_home_to_work}."
+puts "Driving miles from work to Davinci Institude: #{count_miles.miles_from_work_to_davinci}."
+puts "Driving miles from Davinci Institute back home: #{count_miles.miles_from_davinci_to_home}."
