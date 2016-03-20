168a7487aa0a72b2e6152e9409a02850c74f4ab5
Nick Vass <prkarpi@hotmail.com>
2016-03-20 14:13:07 -0600

    updated model file student.rb

--- student_roster_validation/app/models/student.rb
+++ student_roster_validation/app/models/student.rb
@@ -10,13 +10,18 @@ end
 class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\(?\d{1,3}\)?[\-]?\d{3}\-\d{4}/i
-      record.errors[attribute] << (options[:message] || 'is an invalid format')
+      record.errors[attribute] << (options[:message] || 'is an invalid format. Format: 321-555-1234')
     end
   end
 end
 
 class Student < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
-  validates :email, presence: true, email: true
-  validates :cell_phone, :home_phone, :work_phone, presence: true, phone: true
+  validates :email, presence: true
+  validates :email, email: true, if: 'email.present?'
+  validates :cell_phone, phone: true, if: 'cell_phone.present?'
+  validates :home_phone, phone: true, if: 'home_phone.present?'
+  validates :work_phone, phone: true, if: 'work_phone.present?'
+  validates :cell_phone, :home_phone, :work_phone, presence: true
+
 end
